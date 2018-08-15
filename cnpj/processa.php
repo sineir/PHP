266fde5672d7cc9f5aa9ab7ad62d8c4bc3cbb0bd
Sineir Soares <sineir@gmail.com>
2018-08-15 13:32:49 -0300

    Revert "make string sql"
    This reverts commit a44f1b5388f0382c05b950c3044b1834230dfa36.

--- cnpj/processa.php
+++ cnpj/processa.php
@@ -5,29 +5,29 @@
 // para dentro de aplicações web que necessitem da resposta destas consultas para proseguirem, como e-comerce e afins.
 
 require('funcoes.php');
-$field[0] = "cnpj";
-$field[1] = "dat_ini";
-$field[2] = "razao";
-$field[3] = "fantasia";
-$field[4] = "cnae1";
-$field[5] = "cnae2";
-$field[6] = "nat";
-$field[7] = "log";
-$field[8] = "num";
-$field[9] = "comp";
-$field[10] = "cep";
-$field[11] = "bairro";
-$field[12] = "mun";
-$field[13] = "uf";
-$field[14] = "email";
-$field[15] = "tel";
-$field[16] = "efr";
-$field[17] = "sit";
-$field[18] = "dat_sit";
-$field[19] = "mot_sit";
-$field[20] = "sit_esp";
-$field[21] = "dat_sit_esp";
-$field['status'] = "status";
+$field[0] = "CNPJ";
+$field[1] = "Abertura";
+$field[2] = "Nome Empresarial";
+$field[3] = "Nome de Fantasia";
+$field[4] = "Ativ. Principal";
+$field[5] = "Ativ.Secundarias";
+$field[6] = "Nat.Juridica";
+$field[7] = "Logradouro";
+$field[8] = "Numero";
+$field[9] = "Complemento";
+$field[10] = "Cep";
+$field[11] = "Bairro";
+$field[12] = "Municipio";
+$field[13] = "UR";
+$field[14] = "e-mail";
+$field[15] = "Telefone";
+$field[16] = "EFR";
+$field[17] = "Situacao";
+$field[18] = "Data Situacao";
+$field[19] = "Motivo Situacao";
+$field[20] = "Situacao Especial";
+$field[21] = "Data Situacao Especial";
+$field['status'] = "Status";
 
 // dados da postagem de formulário de CNPJ
 $cnpj = $_POST['cnpj'];						// Entradas POST devem ser tratadas para evitar injections
@@ -50,109 +50,19 @@ if($cpf AND $datanascim AND $captcha_cpf)
 }
 //print_r($campos);
 
-$sql = "insert into cnpj (";
-foreach($field as $value){
-	$sql .= $virg.$value;
-	$virg=",";
-}
-$sql .=") values (";
-
 echo "<table border=1>\r\n";
-$virg="";
 foreach ($campos as $key=>$value){
-echo "<tr><td>$key:".$field[$key];
-echo "<td>\r\n";
+echo "<tr><td>".$field[$key]."<td>\r\n";
+
 if (is_array($value)) {
-	$xsql = "";
 	foreach ($value as $kk=>$vv){
-		//echo $pular.$vv;
-		echo $pular.tirar($key,$vv);
-		$xsql .= $pular.tirar($key,$vv);
-		$pular = ",";
+		echo $pular.$vv;
+		$pular = "<br>";
 	}
 	}else{	
-	//echo "$value\r\n";
-	echo tirar($key,$value);
-	$xsql = tirar($key,$value);
-	}
-	$sql .= $virg . chr(34) . $xsql . chr(34) ;
-	$virg=",";
-}
-$sql .= ");";
-echo "</table><br><br>$sql";
-
-function tirar($chave,$text){
-	$arg = str_replace("-","",$text);
-	$arg = str_replace(".","",$arg);
-	$arg = str_replace("/","",$arg);
-	$arg = str_replace("(","",$arg);
-	$arg = str_replace(")","",$arg);
-	$arg = str_replace("*","",$arg);
-	
-	switch ($chave) {
-		case 0; //cnpj len 14
-		case 3; //fantasia sem '*'
-		case 10; //cep len 8
-		case 15; //tel only numbers
-		case 16; //eft sem '*'
-		case 20; //sit esp sem '*'
-		return $arg;
-		break;
-		case 1; //datas yyyy-mm-dd
-		case 18;
-		case 21;
-		if ($arg !="" and substr($arg,0,1)!="*"){
-			$arg = substr($arg,-4)."-".substr($arg,2,2)."-".substr($arg,0,2);
-		}else{
-			$arg = "";
-		}
-		return $arg;
-		break;
-		case 4; //cnae's len 7
-		case 5;
-		return substr($arg,0,7);
-		break;
-		case 6; //nat jur len 4
-		return substr($arg,0,4);
-		break;
-		default:
-		return $text; // demais original
+	echo "$value\r\n";
 	}
 }
-/*
 
-  id,cnpj,dat_ini,razao,fantasia,cnae1,cnae2,nat,log,num,comp,cep,bairro,
-  mun,uf,email,tel,efr,sit,dat_sit,mot_sit,sit_esp,dat_sit_esp,status
-
-
-CREATE TABLE IF NOT EXISTS `cnpj` (
-  `id` bigint(20) NOT NULL,
-  `cnpj` varchar(14) DEFAULT NULL,
-  `dat_ini` datetime DEFAULT NULL,  
-  `razao` varchar(50) DEFAULT NULL,
-  `fantasia` varchar(50) DEFAULT NULL,
-  `cnae1` varchar(7) DEFAULT NULL,
-  `cnae2` varchar(50) DEFAULT NULL,
-  `nat` varchar(4) DEFAULT NULL,
-  `log` varchar(50) DEFAULT NULL,
-  `num` varchar(20) DEFAULT NULL,
-  `comp` varchar(20) DEFAULT NULL,
-  `cep` varchar(8) DEFAULT NULL,
-  `bairro` varchar(30) DEFAULT NULL,
-  `mun` varchar(30) DEFAULT NULL,
-  `uf` varchar(2) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `tel` varchar(12) DEFAULT NULL,
-  `efr` varchar(20) DEFAULT NULL,
-  `sit` varchar(30) DEFAULT NULL,
-  `dat_sit` datetime DEFAULT NULL,
-  `mot_sit` varchar(30) DEFAULT NULL,
-  `sit_esp` varchar(30) DEFAULT NULL,
-  `dat_sit_esp` datetime DEFAULT NULL,
-  `status` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-*/
 
 ?>
